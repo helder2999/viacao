b1e4cccf59a0c082745518b6dc0d893b470d60e4
helder2999 <helder2999@gmail.com>
2020-01-31 09:20:24 -0100

    Add files via upload

---
+++ precache-manifest.a576b4f66b386bd8675fcd9b3ac73fdf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a13080bcba4658c3c063f7fa26509cd",
+    "url": "/img/Coat_of_arms_of_Cape_Verde.3a13080b.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "d0a8a4385b57f00bb5c2",
+    "url": "/css/about.864295f2.css"
+  },
+  {
+    "revision": "4e9bd486688ebdf3dd83",
+    "url": "/css/app.6f081855.css"
+  },
+  {
+    "revision": "4e9bd486688ebdf3dd83",
+    "url": "/js/app.805bf459.js"
+  },
+  {
+    "revision": "8d8f908f5955245e84f6",
+    "url": "/css/chunk-vendors.53087fde.css"
+  },
+  {
+    "revision": "8d8f908f5955245e84f6",
+    "url": "/js/chunk-vendors.eb9dd0fc.js"
+  },
+  {
+    "revision": "f0ca69990c0027836daa",
+    "url": "/js/DetalheModelo2.879be66e.js"
+  },
+  {
+    "revision": "d0a8a4385b57f00bb5c2",
+    "url": "/js/about.378bebed.js"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "/fonts/MaterialIcons-Regular.bca3a187.woff2"
+  },
+  {
+    "revision": "b661c28b0f28606a96722ad2d9588b70",
+    "url": "/fonts/MaterialIcons-Regular.b661c28b.eot"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "/fonts/MaterialIcons-Regular.9219a80f.woff"
+  },
+  {
+    "revision": "586090b38a233ce0201fb221eb117a36",
+    "url": "/fonts/MaterialIcons-Regular.586090b3.ttf"
+  },
+  {
+    "revision": "3165efb5f8f0f602beabded2343318e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0ca69990c0027836daa",
+    "url": "/css/DetalheModelo2.db1fbbe6.css"
+  }
+];
